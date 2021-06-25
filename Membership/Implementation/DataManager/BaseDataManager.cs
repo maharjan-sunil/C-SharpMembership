2c57e4c5e3669dfebb86c2107c3e0600a69a30d0
sunil <sunil@codebee.dk>
2021-06-25 15:47:31 +0545

    Inheritance added with base concept

---
+++ Membership/Implementation/DataManager/BaseDataManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Membership.Implementation.DataManager
+{
+    public class BaseDataManager
+    {
+        protected int ApplicationId;
+        public BaseDataManager()
+        {
+
+        }
+
+        public BaseDataManager(int applicationId)
+        {
+            ApplicationId = applicationId;
+        }
+    }
+}
