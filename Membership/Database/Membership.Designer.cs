4e5bab01a8d8f3766d3a9fee93dc2dfb2d107546
Maharjan-Sunil <histugaya47@gmail.com>
2021-07-07 12:21:02 +0545

    Unit-Exercise fixed aspnet_regsql

--- Membership/Database/Membership.Designer.cs
+++ Membership/Database/Membership.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Internal\Membership\Membership\Database\Membership.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\user\Documents\VisualStudio2015\Projects\Membership\Membership\Database\Membership.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
